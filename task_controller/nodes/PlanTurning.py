572e400a0bc80bb27da3662d0853de861a07c9d9
Calder Phillips-Grafflin <calderpg@fire.(none)>
2013-05-20 14:57:18 -0400

    Added empty package for moving planner code to a package

--- task_controller/nodes/PlanTurning.py
+++ task_controller/nodes/PlanTurning.py
@@ -10,4 +10,15 @@ class PLANTURNING(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo("Trying to plan valve-turning trajectories...")
-        return 'Failure'
+        try:
+            # Call the planning system
+            planning_response = None
+            if (planning_response.error_code.val == planning_response.error_code.SUCCESS):
+                rospy.loginfo("Call to the planning system returned a solution")
+                return 'Success'
+            else:
+                rospy.logerr("Call to the planning system did not return a solution")
+                return 'Failure'
+        except:
+            rospy.logfatal("Failure in calling the planning system")
+            return 'Fatal'
