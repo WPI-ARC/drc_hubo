ac64159d162bc7bceffe69c4fea2c1a8bec3f25c
Calder Phillips-Grafflin <calderpg@fire.(none)>
2013-05-20 15:05:52 -0400

    Started work on service interface

---
+++ hubo_planner/nodes/HuboPlannerInterface.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#############################################################################
+#                                                                           #
+#   Calder Phillips-Grafflin -- WPI/Drexel Darpa Robotics Challenge Team    #
+#                                                                           #
+#   Service interface to the Hubo CBiRRT planner class                      #
+#                                                                           #
+#############################################################################
+
+import roslib; roslib.load_manifest('task_controller')
+import rospy
