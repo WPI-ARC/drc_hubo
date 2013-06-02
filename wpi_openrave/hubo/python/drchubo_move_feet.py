37683526d8533a97c2e015bd3ddab796075cae11
Bener <bener@bener-OptiPlex-980.(none)>
2013-06-02 00:49:58 -0400

    added com calculation and checksupport example in move feet python code for drchubo.

--- wpi_openrave/hubo/python/drchubo_move_feet.py
+++ wpi_openrave/hubo/python/drchubo_move_feet.py
@@ -57,37 +57,34 @@ robots.append(env.ReadRobotURI('../../../openHubo/drchubo/old/drchubo-urdf/robot
 # up-to-date drchubo with joint limits defined
 robots.append(env.ReadRobotURI('../../../openHubo/drchubo/robots/drchubo2.robot.xml'))
 
-# rlhuboplus
-robots.append(env.ReadRobotURI('../../../openHubo/huboplus/rlhuboplus.robot.xml'))
-
 
 # Which robot?
-R = 2
+idx = 0
 
-env.Add(robots[R])
+env.Add(robots[idx])
 
-robots[R].SetTransform(array(MakeTransform(matrix(rodrigues([0,0,0])),transpose(matrix([0.5,0.0,0.0])))))
+robots[idx].SetTransform(array(MakeTransform(matrix(rodrigues([0,pi/12,0])),transpose(matrix([0.0,0.0,0.0])))))
 
-manips = robots[R].GetManipulators()
+manips = robots[idx].GetManipulators()
 
-lowerLimits, upperLimits = robots[R].GetDOFLimits()
+lowerLimits, upperLimits = robots[idx].GetDOFLimits()
 
 print lowerLimits
 print upperLimits
 
 for i in range(35):
-    j = round(robots[R].GetDOFValues([i]),2)
+    j = round(robots[idx].GetDOFValues([i]),2)
     if( (nonzero(lowerLimits[i])[0].size == 0) and (nonzero(j)[0].size == 0) ):
         print "setting joint ",str(i)," to positive zero"
-        robots[R].SetDOFValues([0.000001],[i])
+        robots[idx].SetDOFValues([0.000001],[i])
     elif( (nonzero(upperLimits[i])[0].size == 0) and (nonzero(j)[0].size == 0) ):
         print "setting joint ",str(i)," to negative zero"
-        robots[R].SetDOFValues([-0.000001],[i])
+        robots[idx].SetDOFValues([-0.000001],[i])
 
 probs_cbirrt = RaveCreateModule(env,'CBiRRT')
 
 try:
-    env.AddModule(probs_cbirrt,robots[R].GetName()) # this string should match to <Robot name="" > in robot.xml
+    env.AddModule(probs_cbirrt,robots[idx].GetName()) # this string should match to <Robot name="" > in robot.xml
 except openrave_exception, e:
     print e
     
@@ -96,7 +93,7 @@ probs = env.GetLoadedProblems()
 
 footlinknames = ' Body_RAR Body_LAR '
 
-if( R == 1 or R == 0 ):
+if( idx == 1 or idx == 0 ):
     ## DRCHUBO
     # Center of Gravity Target
     T0_TORSO = manips[5].GetEndEffectorTransform()
@@ -105,22 +102,12 @@ if( R == 1 or R == 0 ):
     cogtarg = [-0.05+T0_TORSO[0,3], 0.085+T0_TORSO[1,3], 0]
 
     T0_LF = manips[2].GetEndEffectorTransform()
-    T0_LF[0,3]+= 0.1
-    T0_LF[2,3]+= 0.3
-    
-    T0_RF = manips[3].GetEndEffectorTransform()
-    T0_RF[0,3]+= 0.1
-    T0_RF[2,3]+= 0.3
-elif( R == 2 ):
-    ## RLHUBOPLUS
-    ## DRCHUBO
-    T0_LF = manips[2].GetEndEffectorTransform()
-    T0_LF[0,3]+= 0.1
+    T0_LF[0,3]+= 0.3
     T0_LF[2,3]+= 0.1
     
     T0_RF = manips[3].GetEndEffectorTransform()
-
-    cogtarg = [-0.05, 0.085, 0]
+    T0_RF[0,3]+= 0.3
+    T0_RF[2,3]+= 0.1
 
 cogtargStr = str(cogtarg).strip("[]").replace(', ',' ')
     
@@ -130,7 +117,7 @@ h.append(misc.DrawAxes(env,T0_LF,0.1))
 h.append(misc.DrawAxes(env,T0_RF,0.1))
 
 goalik = probs[0].SendCommand('DoGeneralIK exec supportlinks 2 '+footlinknames
-+' movecog '+cogtargStr
+#+' movecog '+cogtargStr
 +' nummanips 2 maniptm 2 '+trans_to_str(T0_LF)
 +' maniptm 3 '+trans_to_str(T0_RF)
 )
@@ -139,8 +126,35 @@ print "goalik"
 print goalik
 
 if(goalik != ''):
-    robots[R].SetDOFValues(str2num(goalik),range(35))
+    robots[idx].SetDOFValues(str2num(goalik),range(35))
 
+print "inBalance"
+print probs[0].SendCommand('CheckSupport supportlinks 2 '+footlinknames)
+
+print "let's calculate the COM here..."
+
+# https://en.wikipedia.org/wiki/Center_of_mass
+# R*M = SUM(r_i*m_i)
+#
+# R = SUM(r_i*m_i)/M
+M = 0
+rm = 0
+for l in robots[idx].GetLinks():
+    m = l.GetMass()
+    M += m
+    
+    r = dot(l.GetTransform(),array(MakeTransform(rodrigues([0,0,0]),transpose(matrix(l.GetCOMOffset())))))
+    rm += multiply(r,m)
+
+print "total mass:"
+print M
+
+R = divide(rm,M)
+
+print "COM:"
+print R.round(4)
+
+h.append(misc.DrawAxes(env,array(R.round(4)),0.1))
 
 print "Press Enter to exit..."
 sys.stdin.readline()
